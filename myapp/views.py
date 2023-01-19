0935a6d6fa546305a31ced7e6a13865b3f06cec4
htqkellen <htqkellen@163.com>
2023-01-19 11:41:29 -0500

    added exchange feature

--- myapp/views.py
+++ myapp/views.py
@@ -48,6 +48,7 @@ def exch_rate(request):
         currency2 = request.GET['currency_to']
         c1 = Currency.objects.get(iso=currency1)
         c2 = Currency.objects.get(iso=currency2)
+        support_functions.update_xrates(c1)
         data['currency1'] = c1
         data['currency2'] = c2
         try:
